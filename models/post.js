cc8ef1e312fddec3fdb5926b5c3aaddb8cf585ae
BraianStrak <braianstrak1999@gmail.com>
2021-02-02 19:02:50 +0000

    Added schemas for models

---
+++ models/post.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var PostSchema = new Schema(
+  {
+    title: {type: String, required: true, maxlength: 100},
+    description: {type: String, required: true, maxlength: 100},
+    user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+  }
+);
+
+// Virtual for post's URL
+PostSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/post/' + this._id;
+});
+
+module.exports = mongoose.model('User', UserSchema);
---
+++ models/user.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema(
+  {
+    first_name: {type: String, required: true, maxlength: 100},
+    family_name: {type: String, required: true, maxlength: 100},
+    user_name: {type: String, required: true, maxlength: 100},
+    is_member: {type: Boolean, required: true},
+  }
+);
+
+// Virtual for author's URL
+UserSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/user/' + this._id;
+});
+
+module.exports = mongoose.model('User', UserSchema);
