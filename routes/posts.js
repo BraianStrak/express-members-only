e054dca91e8f0e0bc0d47e2f44d6d0c92af1d027
BraianStrak <braianstrak1999@gmail.com>
2021-02-02 19:36:42 +0000

    created routes

---
+++ routes/posts.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+// GET catalog home page.
+router.get('/', post_controller.index);
+
+// GET request for creating a post. NOTE This must come before routes that display post (uses id).
+router.get('/post/create', post_controller.post_create_get);
+
+// POST request for creating post.
+router.post('/post/create', post_controller.post_create_post);
+
+// GET request to delete post.
+router.get('/post/:id/delete', post_controller.post_delete_get);
+
+// POST request to delete post.
+router.post('/post/:id/delete', post_controller.post_delete_post);
+
+// GET request to update post.
+router.get('/post/:id/update', post_controller.post_update_get);
+
+// POST request to update post.
+router.post('/post/:id/update', post_controller.post_update_post);
+
+// GET request for one post.
+router.get('/post/:id', post_controller.post_detail);
+
+// GET request for list of all post items.
+router.get('/posts', post_controller.post_list);
+
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// GET request for creating a user. NOTE This must come before routes that display user (uses id).
+router.get('/user/create', user_controller.user_create_get);
+
+// POST request for creating user.
+router.post('/user/create', user_controller.user_create_post);
+
+// GET request to delete user.
+router.get('/user/:id/delete', user_controller.user_delete_get);
+
+// POST request to delete user.
+router.post('/user/:id/delete', user_controller.user_delete_post);
+
+// GET request to update user.
+router.get('/user/:id/update', user_controller.user_update_get);
+
+// POST request to update user.
+router.post('/user/:id/update', user_controller.user_update_post);
+
+// GET request for one user.
+router.get('/user/:id', user_controller.user_detail);
+
+// GET request for list of all user items.
+router.get('/users', user_controller.user_list);
+
 
 module.exports = router;
